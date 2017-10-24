956e546464f73c9a91f46a2bdf2cd6bc8b3b8b8f
leonard <leonard@unified-dispatch.com>
2017-10-23 18:12:20 -0700

    worked on drawer

--- app/src/main/java/com/blog/ljtatum/ubuyismile/activity/MainActivity.java
+++ app/src/main/java/com/blog/ljtatum/ubuyismile/activity/MainActivity.java
@@ -1,18 +1,43 @@
 package com.blog.ljtatum.ubuyismile.activity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.app.amazon.framework.utils.AmazonWebServiceAuthentication;
+import com.app.framework.utilities.DeviceUtils;
+import com.app.framework.utilities.FrameworkUtils;
 import com.blog.ljtatum.ubuyismile.R;
+import com.blog.ljtatum.ubuyismile.fragments.AboutFragment;
+import com.blog.ljtatum.ubuyismile.fragments.PrivacyFragment;
 
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    private Context mContext;
+    private Activity mActivity;
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.drawer);
+
+        // initialize views and listeners
+        initializeViews();
+        initializeHandlers();
+        initializeListeners();
 
 
         AmazonWebServiceAuthentication authentication = AmazonWebServiceAuthentication.create(
@@ -22,4 +47,136 @@ public class MainActivity extends BaseActivity {
 
     }
 
+    /**
+     * Method is used to initialize views
+     */
+    private void initializeViews() {
+        mContext = MainActivity.this;
+        mActivity = MainActivity.this;
+
+        // drawer
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, mDrawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+
+            @Override
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                if (FrameworkUtils.checkIfNull(getTopFragment())) {
+                    // show keyboard
+                    DeviceUtils.showKeyboard(mContext);
+                }
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                // hide keyboard
+                DeviceUtils.hideKeyboard(mContext, getWindow().getDecorView().getWindowToken());
+            }
+        };
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    /**
+     * Method is used to set click listeners
+     */
+    private void initializeHandlers() {
+        // navigation drawer
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setItemIconTintList(null);
+        setupDrawerIcons(navigationView);
+    }
+
+    /**
+     * Method is used to initialize listeners and callbacks
+     */
+    private void initializeListeners() {
+
+    }
+
+
+    /**
+     * Method is used to setup drawer icons
+     */
+    private void setupDrawerIcons(NavigationView navigationView) {
+        Menu menu = navigationView.getMenu();
+        // TODO manage menu icons
+    }
+
+    /**
+     * Method is used to enable/disable drawer
+     *
+     * @param isEnabled
+     */
+    public void toggleDrawerState(boolean isEnabled) {
+        if (!FrameworkUtils.checkIfNull(mDrawerLayout)) {
+            if (isEnabled) {
+                // only unlock (enable) drawer interaction if it is disabled
+                if (mDrawerLayout.getDrawerLockMode(GravityCompat.START) != DrawerLayout.LOCK_MODE_UNLOCKED) {
+                    mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
+                }
+            } else {
+                // only allow disabling of drawer interaction if the drawer is closed
+                mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+            }
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        switch (item.getItemId()) {
+            case R.id.nav_browse:
+
+                break;
+            case R.id.nav_search:
+
+                break;
+            case R.id.nav_settings:
+
+                break;
+            case R.id.nav_all_good_deals:
+
+                break;
+            case R.id.nav_quick_search_books:
+
+                break;
+            case R.id.nav_quick_search_electronics:
+
+                break;
+            case R.id.nav_quick_search_food:
+
+                break;
+            case R.id.nav_quick_search_health_beauty:
+
+                break;
+            case R.id.nav_quick_search_movies:
+
+                break;
+            case R.id.nav_quick_search_video_games:
+
+                break;
+            case R.id.nav_about:
+                fragment = new AboutFragment();
+                break;
+            case R.id.nav_privacy:
+                fragment = new PrivacyFragment();
+                break;
+            default:
+                break;
+        }
+        // add fragment
+        if (!FrameworkUtils.checkIfNull(fragment)) {
+            addFragment(fragment);
+        }
+
+        // close drawer after selection
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return false;
+    }
 }
