c4fdb628139b24182a07c9919525d282f9abf2a4
Leonard Tatum <ljtatum@hotmail.com>
2017-12-31 20:01:05 -0800

    adapter started part 2 of 2

---
+++ app/src/main/java/com/blog/ljtatum/ubuyismile/adapter/ChableeItemAdapter.java
@@ -0,0 +1,92 @@
+package com.blog.ljtatum.ubuyismile.adapter;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.app.framework.utilities.FrameworkUtils;
+import com.blog.ljtatum.ubuyismile.R;
+import com.blog.ljtatum.ubuyismile.model.ChableeData;
+import com.blog.ljtatum.ubuyismile.model.ChableeModel;
+import com.blog.ljtatum.ubuyismile.utils.Utils;
+
+import java.util.ArrayList;
+
+/**
+ * Created by LJTat on 12/31/2017.
+ */
+
+public class ChableeItemAdapter extends RecyclerView.Adapter<ChableeItemAdapter.ViewHolder> {
+
+    private final Context mContext;
+    private ArrayList<ChableeModel> alItems;
+
+    /**
+     * Constructor
+     * @param context Interface to global information about an application environment
+     * @param alItems List of items to display
+     */
+    public ChableeItemAdapter(Context context, ArrayList<ChableeModel> alItems) {
+        mContext = context;
+        this.alItems = alItems;
+    }
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(mContext).inflate(R.layout.item_a, parent, false);
+        return new ChableeItemAdapter.ViewHolder(v);
+    }
+
+    @SuppressLint("StringFormatInvalid")
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        final int index = holder.getAdapterPosition();
+
+        // set visibility
+        if ((Utils.getDollarValue(alItems.get(position).salePrice) > 0) &&
+                (Utils.getDollarValue(alItems.get(position).salePrice) <
+                Utils.getDollarValue(alItems.get(position).price))) {
+            FrameworkUtils.setViewVisible(holder.tvSalePerc);
+            // set value
+            holder.tvSalePerc.setText(mContext.getResources().getString(R.string.percent_sale,
+                    String.valueOf(Utils.calculatePercSale(Utils.getDollarValue(alItems.get(position).price),
+                    Utils.getDollarValue(alItems.get(position).salePrice)))));
+        }
+
+        // set values
+
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return alItems.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView tvSalePerc, tvLabel, tvTitle, tvPrice;
+        private final ImageView ivBg;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvSalePerc = itemView.findViewById(R.id.tv_sale_perc);
+            tvLabel = itemView.findViewById(R.id.tv_label);
+            tvTitle = itemView.findViewById(R.id.tv_title);
+            tvPrice = itemView.findViewById(R.id.tv_price);
+            ivBg = itemView.findViewById(R.id.iv_bg);
+        }
+
+
+    }
+}
